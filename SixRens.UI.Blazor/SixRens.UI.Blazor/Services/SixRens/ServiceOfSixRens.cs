6f388c592b5e0869115cf8456023645fd553d07d
yueyinqiu <lujiaqi2003@126.com>
2022-08-11 16:41:41 +0800

    彻底感受到了失败

--- SixRens.UI.Blazor/SixRens.UI.Blazor/Services/SixRens/ServiceOfSixRens.cs
+++ SixRens.UI.Blazor/SixRens.UI.Blazor/Services/SixRens/ServiceOfSixRens.cs
@@ -5,7 +5,7 @@ using TG.Blazor.IndexedDB;
 
 namespace SixRens.UI.Blazor.Services.SixRens
 {
-    public sealed partial class ServiceOfSixRens
+    public sealed partial class ServiceOfSixRens : IDisposable
     {
         public static IEnumerable<StoreSchema> IndexedDbStoreSchemas
         {
@@ -15,7 +15,6 @@ namespace SixRens.UI.Blazor.Services.SixRens
                 yield return PresetSaver.IndexedDbStoreSchema;
             }
         }
-
 
         private 插件包管理器? pluginPackageManager;
         public async Task<插件包管理器> GetPluginPackageManager()
@@ -39,10 +38,48 @@ namespace SixRens.UI.Blazor.Services.SixRens
             return this.presetManager;
         }
 
+        public async Task<bool> InstallDefaultPlugins()
+        {
+            using var s = await
+                httpClient.GetStreamAsync("plugin-packages/SixRens.DefaultPlugins-1.14.1.srspg");
+            using MemoryStream ms = new();
+            await s.CopyToAsync(ms);
+            ms.Position = 0;
+            var manager = await GetPluginPackageManager();
+            logger.LogError("BEOFRE");
+            var (_, 未加入) = manager.从外部加载插件包(ms);
+            logger.LogError("AFTER");
+            return !未加入;
+        }
+
+        public async Task InstallDefaultPresets()
+        {
+            var s = await
+               httpClient.GetStringAsync("default-presets/DefaultPreset.bin");
+            var manager = await GetPresetManager();
+            var p = manager.导入预设文件内容("默认预设", s);
+            for (; p is null;)
+            {
+                p = manager.导入预设文件内容($"默认预设（{Guid.NewGuid()}）", s);
+            }
+        }
+
+        public void Dispose()
+        {
+            this.pluginPackageManager?.Dispose();
+        }
+
         private readonly IndexedDBManager dBManager;
-        public ServiceOfSixRens(IndexedDBManager dBManager)
+        private readonly HttpClient httpClient;
+        private readonly ILogger<ServiceOfSixRens> logger;
+        public ServiceOfSixRens(
+            IndexedDBManager dBManager,
+            HttpClient httpClient, 
+            ILogger<ServiceOfSixRens> logger)
         {
             this.dBManager = dBManager;
+            this.httpClient = httpClient;
+            this.logger = logger;
         }
     }
 }
